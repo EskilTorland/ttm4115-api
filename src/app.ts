699e83b4a00f3d8fa240fffd4dc2c97b8a32e598
Eskil Torland <eskil.torland@gmail.com>
2022-05-02 14:00:01 +0200

    Requests can be made

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ const app = express()
 dotenv.config()
 import {Pool} from 'pg'
 import https from 'https'
+import db from "./queries"
+import pg from "pg-promise/typescript/pg-subset"
+import { executionAsyncResource } from "async_hooks"
 
 const pool = new Pool({
     host: process.env.DB_HOST,
@@ -13,6 +16,62 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || "5432")
 })
+
+
+const execute = async(query:string) =>{
+    try{
+        await pool.connect()
+        await pool.query(query)
+        return true
+    }catch(e){
+        console.error(e)
+        return false
+    }
+}
+
+const createQuizTable = `
+CREATE TABLE IF NOT EXISTS "quiz" (
+    "id" SERIAL,
+     "question" VARCHAR(512) NOT NULL,
+     "answers" VARCHAR(1000) NOT NULL
+);`
+
+execute(createQuizTable).then(result =>{
+    if(result){
+        console.log("Table created")
+    }
+})
+
+const insertQuiz = async(question:string,answers:string) =>{
+    try{
+        await pool.connect()
+        await pool.query(
+            `INSERT INTO "quiz" ("question", "answers")
+             VALUES ($1, $2)`, [question,answers]
+        )
+        return true
+    } catch(e){
+        console.error(e)
+    }
+}
+
+insertQuiz("spm","ans").then(result =>{
+    if(result){
+        console.log("Quiz inserted2")
+    }
+})
+
+const fetchQuiz = async() =>{
+    try {
+       await pool.connect()
+       return await pool.query(`SELECT * FROM "quiz"`)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+fetchQuiz()
 
 const connectToDb = async () => {
     try {
@@ -31,14 +90,20 @@ const req = https.get("https://opentdb.com/api.php?amount=10&type=multiple",res 
     })
     res.on('end', () => {
         let response = JSON.parse(data)
-        let test = response.results[1]
-        console.log(test.type)
+        for(let i = 0; i < response.results.length; i++){
+
+        }
+        let test = response.results.length
+        console.log(test)
     })
 })
 
-
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     res.send("hi")
+})
+app.get("/getQuiz", (req: Request, res: Response, next: NextFunction) =>{
+    fetchQuiz().then(result => console.log(result?.rows))
+    fetchQuiz().then(result => res.send(result?.rows))
 })
 
 app.listen(process.env.PORT, () => {
---
+++ src/queries.d.ts
@@ -0,0 +1,96 @@
+import {Pool} from 'pg'
+import {Response, Request, response} from 'express'
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || "5432")
+})
+
+const createQuizTableQuery = `
+CREATE TABLE IF NOT EXISTS "quiz" (
+    "id" SERIAL,
+     "question" VARCHAR(512) NOT NULL,
+     "answers" VARCHAR(1000) NOT NULL
+);`
+
+const createTeamTableQuery = `
+    CREATE TABLE IF NOT EXISTS "teams" (
+        "id" SERIAL,
+        "name" VARCHAR(128) NOT NULL,
+        "score" INT
+    )
+`
+
+const createQuizTable = async () => {
+    try {
+        pool.query(createQuizTableQuery)
+    }catch(e){
+        console.error(e)
+    }
+}
+
+const createTeamTable = async () => {
+    try {
+        pool.query(createTeamTableQuery)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const getQuiz = (request:Request, response:Request) =>{
+    pool.query(`SELECT * FROM quiz`), (error,results) => {
+        response.statusCode(200).json(results.rows)
+       }
+
+    }
+
+
+const updateQuiz = async() =>{
+    try {
+        await pool.query(`DELETE * FROM quiz`)
+        await pool.query(
+            `INSERT INTO quiz (question, answers)
+             VALUES ($1, $2)`, [question,answers]
+        )
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const getTeams = async () =>{
+    try {
+        await pool.query(`SELECT * FROM teams`)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const updateTeam = async () =>{
+    try {
+        
+        await pool.query(`
+        IF EXISTS (SELECT * FROM teams WHERE name = $2)
+        BEGIN
+        UPDATE teams
+        SET score = score + $1 
+        WHERE team = $2
+        END
+        ELSE
+        BEGIN
+        INSERT INTO teams (name, score) VALUES ($2,$1)
+        END`,
+         [score,teamName])
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports = {
+    getQuiz,
+    updateQuiz
+}
+
+
